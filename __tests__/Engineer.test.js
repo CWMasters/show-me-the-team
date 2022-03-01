d902ff50a5a2cf36f7eea9781f43e29be16150aa
CWMasters <95546410+CWMasters@users.noreply.github.com>
2022-03-01 11:12:58 -0700

    Intern passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -6,6 +6,12 @@ test('creates Engineer object', () => {
     expect(engineer.getGithub()).toEqual(expect.any(String));
 });
 
+// gets github
+test('creates github', () => {
+    const engineer = new Engineer('Chris', 10, 'chrismasters_326@outlook.com', 'KJS');
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+})
+
 // get Engineer role
 test('creates Engineer role', () => {
     const engineer = new Engineer('Chris', 10, 'chrismasters_326@outlook.com', 'CWMasters');
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,3 +1,20 @@
 const Intern = require('../lib/Intern.js')
 
-// get School
+// creates Intern object
+test('creates Intern object', () => {
+    const intern = new Intern('Chris', 10, 'chrismasters_326@outlook.com', 'KJS');
+    expect(intern.getSchool()).toEqual(expect.any(String));
+});
+
+// gets school
+test('creates school', () => {
+    const intern = new Intern('Chris', 10, 'chrismasters_326@outlook.com', 'KJS');
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+})
+
+// gets Intern role
+test('creates Intern role', () => {
+    const intern = new Intern('Chris', 10, 'chrismasters_326@outlook.com', 'KJS');
+    expect(intern.getRole()).toEqual('Intern')
+})
+
